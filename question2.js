7c918f2742c4ab25464b4d96f32f15464a032863
Eric Mugambi Gikonyo <eric.gikonyo@student.moringaschool.com>
2025-05-26 17:17:05 +0300

    Question 2 Program

---
+++ question2.js
@@ -0,0 +1,9 @@
+// Replace this with any number you want to test
+let number = 7;
+
+// Check if the number is even or odd
+if (number % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
