9e38b4875d6a4f89217b5d4fc95e46e4d49a072d
Eric Mugambi Gikonyo <eric.gikonyo@student.moringaschool.com>
2025-05-26 17:22:31 +0300

    Grading Program

---
+++ question3.js
@@ -0,0 +1,13 @@
+let score = 85;  // Change this value to test different scores
+
+if (score >= 90 && score <= 100) {
+  console.log("A");
+} else if (score >= 80 && score <= 89) {
+  console.log("B");
+} else if (score >= 70 && score <= 79) {
+  console.log("C");
+} else if (score < 70) {
+  console.log("Fail");
+} else {
+  console.log("Invalid score");
+}
