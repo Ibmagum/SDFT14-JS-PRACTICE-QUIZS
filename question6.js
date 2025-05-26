7acb532c8beab974f5eee15027c3bec6bfb190f3
Eric Mugambi Gikonyo <eric.gikonyo@student.moringaschool.com>
2025-05-26 18:22:16 +0300

    Login Simulation

---
+++ question6.js
@@ -0,0 +1,8 @@
+let username = "admin";  // Change to test
+let password = "1234";   // Change to test
+
+if (username === "admin" && password === "1234") {
+  console.log("Access granted");
+} else {
+  console.log("Access denied");
+}
